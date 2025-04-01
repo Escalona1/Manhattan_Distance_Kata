a0f55989b58abf88175938d6124b129a91235e9c
Escalona1 <chistoseitor@gmail.com>
2025-04-01 18:04:28 -0300

    5 GREEN

--- tests/source/manhattan_distance.py
+++ tests/source/manhattan_distance.py
@@ -5,7 +5,7 @@ class Point:
         self.y = y
 
     def manhattanDistance(self, other):
-        return other.x - self.x + abs(self.y - other.y)
+        return abs(self.x - other.x) + abs(self.y - other.y)
 
 
 def manhattanDistance(Point1, Point2):
