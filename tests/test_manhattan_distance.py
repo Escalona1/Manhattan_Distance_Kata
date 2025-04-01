af8cfcb90af1b6d1da6eb6b3395eccf2ba2192fe
Escalona1 <chistoseitor@gmail.com>
2025-04-01 17:53:16 -0300

    4 RED

--- tests/test_manhattan_distance.py
+++ tests/test_manhattan_distance.py
@@ -9,3 +9,10 @@ def test_aligned_points():
     assert manhattanDistance(p1, p2) == 2
     assert manhattanDistance(p1, p3) == 4
     assert manhattanDistance(p3, p1) == 4
+
+def test_not_aligned_points():
+
+    p1 = Point(1,1)
+    p2 = Point(3,3)
+
+    assert manhattanDistance(p1, p2) == 4
