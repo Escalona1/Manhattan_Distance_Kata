544c85d7cf90cec274642b7c3f03a733e8ba6408
Escalona1 <chistoseitor@gmail.com>
2025-04-01 17:36:45 -0300

    2 RED

--- tests/test_manhattan_distance.py
+++ tests/test_manhattan_distance.py
@@ -1,8 +1,10 @@
 from source.manhattan_distance import Point, manhattanDistance
 
 
-def test_1():
+def test_aligned_points():
     p1 = Point(1,1)
-    p2 = Point(2,2)
+    p2 = Point(1,3)
+    p3 = Point(1,5)
 
-    assert  manhattanDistance(p1, p2) == 1
+    assert manhattanDistance(p1, p2) == 2
+    assert manhattanDistance(p1, p3) == 4
